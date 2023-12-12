5a7e886a016b0e87fbd3c2689a961c68f7a290ff
royalpandit17 <153072890+royalpandit17@users.noreply.github.com>
2023-12-12 15:31:40 +0530

    Update date_time_userbot_teletips.py

--- date_time_userbot_teletips.py
+++ date_time_userbot_teletips.py
@@ -39,7 +39,7 @@ async def main_teletips():
                 Image_edit_teletips = ImageDraw.Draw(Image_teletips)
                 Image_edit_teletips.text((690, 550), Image_text_teletips, (0, 255, 255), font = Image_font_teletips)
                 Image_teletips.save("Image_final_teletips.jpg")
-                await Date_Time_Userbot_teletips.update_profile(bio = f"{Emojis_teletips} {Quotes_teletips}" , last_name = f"| ⏳️ {Time_teletips} | 🗓 {Date_teletips}")
+                await Date_Time_Userbot_teletips.update_profile(bio = f"{Emojis_teletips} {Quotes_teletips}" , last_name = f"| {Time_teletips} | {Date_teletips}")
                 await Date_Time_Userbot_teletips.set_profile_photo(photo="Image_final_teletips.jpg")
                 me = await Date_Time_Userbot_teletips.get_me()
                 photos = Date_Time_Userbot_teletips.get_chat_photos("me")
